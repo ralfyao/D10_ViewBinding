10f0ca4d8ad52f69b1bff52d6c95ba428c8cc4e9
ralfyao <ralfyao@yjfcasting.com>
2024-07-11 16:29:18 +0800

    ADD

--- app/src/main/java/com/yjfcasting/qrcode/d10/MainActivity.java
+++ app/src/main/java/com/yjfcasting/qrcode/d10/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yjfcasting.qrcode.d10;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.yjfcasting.qrcode.d10.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        binding.button.setOnClickListener(view->{
+            binding.textView.setText(binding.etContent.getText().toString());
+        });
+        binding.btnFragment.setOnClickListener(view->{
+            Intent intent = new Intent(MainActivity.this, Activity2.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
